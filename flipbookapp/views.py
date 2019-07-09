c34f63885bfea8027c105d8ab207d331928d2442
Lance Smith <lance.matthew.smith@gmail.com>
2019-07-08 22:39:00 -0700

    Setup views and static assets

--- flipbookapp/views.py
+++ flipbookapp/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Collection, User
 
 
 def home(req):
     return HttpResponse("App home. Hello!")
+
+
+def collection_details(req, collection_id):
+    collection = Collection.objects.get(id=collection_id)
+    context = {collection_id: {"id": collection_id, "title": collection.title}}
+    # return render(req, "flipbookapp/index.html", context)
+    return HttpResponse(f"Details for collection #{context}")
 
 # Create your views here.
